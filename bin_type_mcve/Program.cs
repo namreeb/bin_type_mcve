d75d0fd093d7aa39db8558e052c1fcf0a614c86e
namreeb <github@namreeb.org>
2017-06-02 11:56:15 -1000

    Initial commit.
    Minimum, complete, verifiable example for this question on stackoverflow.com: https://stackoverflow.com/questions/44337501/get-type-of-binary-on-filesystem-via-c-sharp-running-in-64-bit

---
+++ bin_type_mcve/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace bin_type_mcve
+{
+    class Program
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetBinaryTypeW([MarshalAs(UnmanagedType.LPWStr)] string path, out UInt32 result);
+
+        [DllImport("kernel32.dll")]
+        private static extern uint GetLastError();
+
+        static void Main(string[] args)
+        {
+            if (!GetBinaryTypeW("target.exe", out UInt32 result))
+                Console.WriteLine($"GetBinaryTypeW failed.  Errno = {GetLastError()}");
+            else
+                Console.WriteLine($"GetBinaryTypeW succeeded.  Type = {result}");
+        }
+    }
+}
---
+++ target/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "sizeof(void *): " << sizeof(void *) << std::endl;
+    return 0;
+}
